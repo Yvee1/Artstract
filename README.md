5ec70c0f9ecc917e7b01d60ae726c519f135cf09
Tristan Trouwen <tristan.trouwen@gmail.com>
2021-12-04 16:02:09 +0100

    Added stubs and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Artstract
+> Interactive Alpha-shape Art
+
+### Project structure
+
+1. `game.js` contains all geometric algorithms
+2. `app.js` contains code involving the user interface
+3. `index.html` contains the structure and user interface structure
+4. `style.css` contains the styling of the user interface 
--- game.js
+++ game.js
@@ -13,26 +13,95 @@ function handleImageFile(file){
   reader.readAsDataURL(file);
 }
 
-// const app = new PIXI.Application({ antialias: true, view: canvas, transparent: true });
+class Coordinate {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
-// const graphics = new PIXI.Graphics();
+/**
+ * Returns array of k colors present in image.
+ * @param {Element} image 
+ * @param {Number} k
+ * @return {Array} colors
+ * 
+ * Implementation: Martijn
+ */
+function getColorPalette(image, k) {
+  return [[0,0,0]];
+}
 
-// // Circle
-// graphics.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
-// graphics.beginFill(0xDE3249, 1);
-// graphics.drawCircle(100, 250, 10);
-// graphics.endFill();
+/**
+ * Returns edges to draw Delaunay triangulation.
+ * @param {Array<Coordinate>} P
+ * 
+ * Implementation: Tristan
+ */
+function getDelaunayTriangulation(P){
+  return [[0,1]];
+}
 
-// // Circle + line style 1
-// graphics.lineStyle(2, 0xFEEB77, 1);
-// graphics.beginFill(0x650A5A, 1);
-// graphics.drawCircle(250, 250, 50);
-// graphics.endFill();
+/**
+ * 
+ * @param {Coordinate} p 
+ * @param {Array<Coordinate>} T 
+ */
+function insert(p, T)
 
-// // Circle + line style 2
-// graphics.lineStyle(10, 0xFFBD01, 1);
-// graphics.beginFill(0xC34288, 1);
-// graphics.drawCircle(400, 250, 50);
-// graphics.endFill();
+/**
+ * Legalizes edge (pi, pj) if illegal.
+ * @param {Coordinate} pr 
+ * @param {Coordinate} pi 
+ * @param {Coordinate} pj 
+ * @param {Array<Coordinate>} T 
+ * 
+ * Implementation: Tristan
+ */
+function legalizeEdge(pr, pi, pj, T) {
+  return T;
+}
 
-// app.stage.addChild(graphics);
+/**
+ * Returns alpha-shape edges
+ * @param {Array<Coordinate>} P - input set
+ * @param {Number} alpha
+ * 
+ * Implementation: Steven
+ */
+function getAlphaShape(P, alpha) {
+  return [[0,1]];
+}
+
+/**
+ * Draws art to canvas.
+ * 
+ * Implementation: Martijn
+ */
+function drawArt() {
+
+}
+
+const app = new PIXI.Application({ antialias: true, view: canvas, transparent: true });
+
+const graphics = new PIXI.Graphics();
+
+// Circle
+graphics.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
+graphics.beginFill(0xDE3249, 1);
+graphics.drawCircle(100, 250, 10);
+graphics.endFill();
+
+// Circle + line style 1
+graphics.lineStyle(2, 0xFEEB77, 1);
+graphics.beginFill(0x650A5A, 1);
+graphics.drawCircle(250, 250, 50);
+graphics.endFill();
+
+// Circle + line style 2
+graphics.lineStyle(10, 0xFFBD01, 1);
+graphics.beginFill(0xC34288, 1);
+graphics.drawCircle(400, 250, 50);
+graphics.endFill();
+
+app.stage.addChild(graphics);
