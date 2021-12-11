26a071e13de48bea0eeb0755422d3869e48be441
Yvee1 <stevenvdb@live.nl>
2021-12-11 12:34:06 +0100

    Continued on alpha shapes. Still need to construct non-simple polygons where needed.

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ const options = {
 const alphaController = gui.add(options, 'alpha');
 alphaController.onChange(() => drawStuff());
 
-const r = 2;
+const r = 3;
 
 class Coordinate {
   constructor(x, y) {
@@ -93,29 +93,44 @@ function circumradius(p1, p2, p3) {
  * 
  * Implementation: Steven
  */
-function getAlphaShape(del, coords, alpha) {
+function getAlphaShape(del, coords, alpha){
+
+}
+
+function getAlphaShapePerimeterEdges(del, coords, alpha) {
+  // Assumption: for every triangle the vertices are in counter-clockwise order.
+
+  // Shorthand
   const triangles = del.triangles;
+
+  // We will filter the triangles based on alpha, and determine the perimeter edges of the resulting triangulation(s).
   const allEdges = [];
-  const perimeterEdges = [];
+  let perimeterEdges = [];
 
   for (let i = 0; i < triangles.length; i+=3) {
+    // Indices of the triangle vertices
     const t0 = triangles[i];
     const t1 = triangles[i+1];
     const t2 = triangles[i+2];
+    // Coordinates of the triangle vertices
     const p1 = coords[t0];
     const p2 = coords[t1];
     const p3 = coords[t2];
+    // Calculate circumradius; should be < alpha to be in the alpha shape.
     const r = circumradius(p1, p2, p3);
     if (r < alpha) { 
+      // These are the edges of the triangle, we'll determine which are perimeter edges.
       const edges = [[t0, t1], [t1, t2], [t2, t0]];
       for (let i = 0; i < edges.length; i++) {
         const edge = edges[i];
-        const flippedEdge = edge.reverse();
+        const flippedEdge = edge.slice().reverse();
+        // If this is the first time we encounter this edge (regardless of direction) it is a candidate perimeter edge.
         if (!allEdges.some(e => (e[0] == edge[0] && e[1] == edge[1]) || (e[0] == flippedEdge[0] && e[1] == flippedEdge[1]))){
           allEdges.push(edge);
           perimeterEdges.push(edge);
-        } else {
-          console.log(":)")
+        } else { 
+          // An edge is a perimeter edge iff we encounter it exactly once. 
+          //So if we have encoutnered the edge before, it is no longer a candidate perimeter edge, therefore remove it.
           perimeterEdges = perimeterEdges.filter(e => !(e[0] == edge[0] && e[1] == edge[1]) && !(e[0] == flippedEdge[0] && e[1] == flippedEdge[1]));
         }
       }
@@ -123,6 +138,94 @@ function getAlphaShape(del, coords, alpha) {
   }
   
   return perimeterEdges;
+}
+
+function ccwAngle(p1, p2, p3){
+  // Angle of below would be approx. 5/4*pi
+  //                p3
+  //              /
+  //            /
+  // p1 ----- p2
+  const x1 = p2.x - p1.x;
+  const y1 = p2.y - p1.y;
+  const x2 = p3.x - p2.x;
+  const y2 = p3.y - p2.y;
+  const dot = x1 * x2 + y1 * y2;
+  const det = x1 * y2 - y1 * x2;
+  return Math.atan2(dot, det);
+}
+
+function perimeterEdgesToPolygons(edges){
+  // We first create a mapping from the edges. If vertex u has outgoing edges (u, v) and (u, w) then u is mapped to [v, w].
+  const mapping = new Map();
+  const polygons = [];
+  const remainingVertices = new Set();
+
+  for (let i = 0; i < edges.length; i++){
+    if (mapping.has(edges[i][0])){
+      mapping.get(edges[i][0]).push(edges[i][1]);
+    } else {
+      mapping.set(edges[i][0], [edges[i][1]]);
+    }
+    remainingVertices.add(edges[i][0]);
+  }
+
+  while(remainingVertices.size > 0){
+    const polygon = [];
+    // Get a vertex from the set
+    const start = remainingVertices.values().next().value;
+    polygon.push(start);
+    let current = start;
+    let prev = -1;
+    for (let i = 0; i < edges.length; i++){ // while(true) but just to prevent it going into an infinite loop in case of a mistake
+      // Which vertices can be reached from current?
+      const possibleNexts = mapping.get(current);
+      // Get the one with the smallest angle; this is the one in the same polygon.
+      const next = prev < 0 ? possibleNexts[0] : possibleNexts.reduce((acc, x) => ccwAngle(prev, current, x) < ccwAngle(prev, current, acc) ? x : acc);
+      // Remove it from the list
+      possibleNexts.splice(possibleNexts.indexOf(next), 1);
+
+      polygon.push(next);
+      remainingVertices.delete(current);
+      prev = current;
+      current = next;
+      // If we found a closed polygon we stop going around.
+      if (current == start){
+        break;
+      }
+    }
+    polygons.push(polygon);
+  }
+  return polygons;
+}
+
+function pointInsidePolygon(point, poly){
+  // TODO
+}
+
+function polygonsToNestedPolygons(polygons){
+  const nestedPolygons = [];
+  const inner = new Set();
+  for (let i = 0; i < polygons.length; i++){
+    if (inner.has(i)){
+      continue;
+    }
+    const p1 = polygons[i];
+    nestedPolygons.push([p1]);
+    for (let j = 0; i < polygons.length; j++){
+      if (i == j){
+        continue;
+      }
+      const p2 = polygons[j];
+      if (p2.some(point => !pointInsidePolygon(point, p1))){
+        continue;
+      }
+      // Now: p2 nested in p1
+      inner.add(j);
+      nestedPolygons[nestedPolygons.length - 1].push(p2);
+    }
+  }
+  return nestedPolygons;
 }
 
 /**
@@ -176,7 +279,7 @@ function drawStuff() {
     }
   }
 
-  const fewerPoints = points.filter(p => Math.random() < 0.1);
+  const fewerPoints = points.filter(p => Math.random() < 0.05);
 
   // Draw points
   drawPoints(fewerPoints);
@@ -193,13 +296,35 @@ function drawStuff() {
   //   ctx.stroke();
   // }
 
+  const perimeterEdges = getAlphaShapePerimeterEdges(del, coordList, options.alpha);
+  const polygons = perimeterEdgesToPolygons(perimeterEdges);
 
-  const perimeterEdges = getAlphaShape(del, coordList, options.alpha);
-  // Draw alpha shape
+  // Draw perimeter edges of alpha shape
   perimeterEdges.forEach(edge => {
+    ctx.fillStyle = 'black';
     ctx.beginPath();
     ctx.moveTo(coordList[edge[0]].x, coordList[edge[0]].y);
     ctx.lineTo(coordList[edge[1]].x, coordList[edge[1]].y);
     ctx.stroke();
+    ctx.font = "10px Arial";
+    ctx.fillText(edge[0], coordList[edge[0]].x, coordList[edge[0]].y); 
+    ctx.fillText(edge[1], coordList[edge[1]].x, coordList[edge[1]].y); 
   })
+
+  polygons.forEach(polygon => {
+    const firstPoint = fewerPoints[polygon[0]];
+    ctx.fillStyle = `rgb(${firstPoint.color.r}, ${firstPoint.color.g}, ${firstPoint.color.b}, 0.5)`;
+    ctx.beginPath();
+    ctx.moveTo(coordList[polygon[0]].x, coordList[polygon[0]].y);
+    for (let i = 1; i < polygon.length; i++){
+      ctx.lineTo(coordList[polygon[i]].x, coordList[polygon[i]].y);
+    }
+    ctx.fill();
+  })
+
+  console.log("Polygons");
+  console.log(polygons);
+  // const nestedPolygons = polygonsToNestedPolygons(polygons);
+  // console.log("Nested polygons");
+  // console.log(nestedPolygons);
 }
