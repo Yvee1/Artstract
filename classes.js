d43a5a082edfd8a3d31abb24b5a3961a83e2d386
Tristan Trouwen <tristan.trouwen@gmail.com>
2021-12-14 09:14:49 +0100

    Added bug fixed which disappeared in merge

--- classes.js
+++ classes.js
@@ -6,13 +6,13 @@ class Coordinate {
 
   /* Returns whether this coordinate lies in the triangle */
   isInTriangle(triangle, coordList) {
-    p1 = coordList[triangle[0]];
-    p2 = coordList[triangle[1]];
-    p3 = coordList[triangle[2]];
+    const p1 = coordList[triangle[0]];
+    const p2 = coordList[triangle[1]];
+    const p3 = coordList[triangle[2]];
 
     const d1 = sign(this, p1, p2);
     const d2 = sign(this, p2, p3);
-    const d3 = sign(this, p1, p3);
+    const d3 = sign(this, p3, p1);
 
     const hasNeg = (d1 < 0) || (d2 < 0) || (d3 < 0);
     const hasPos = (d1 > 0) || (d2 > 0) || (d3 > 0);
